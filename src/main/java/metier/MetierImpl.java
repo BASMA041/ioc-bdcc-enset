2229af61472adeec6da66cb7079efc100d1e3867
BASMA041 <128806699+BASMA041@users.noreply.github.com>
2025-04-17 13:01:06 +0200

    commit

--- src/main/java/metier/MetierImpl.java
+++ src/main/java/metier/MetierImpl.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
+import ext.DaoImplV2;
 @Service("metier")
 
 
